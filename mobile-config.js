a47e4f25758707e84cae5f51fc0df9a60115cd19
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-01-30 14:25:55 +0900

    Add iOS App config

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+// mobile-config.js
+App.info({
+  id: 'com.example.meteor-todo-app', // アプリID
+  name: 'Todo',                      // アプリ名
+  description: 'Todo App',           // アプリ説明
+  author: 'Example Inc.',            // 作者
+  email: 'contact@example.com',      // メールアドレス
+  website: 'http://example.com',     // ウェブサイト
+  version: '0.1.0',                  // バージョン番号
+  buildNumber: '1'                   // ビルド番号
+});
